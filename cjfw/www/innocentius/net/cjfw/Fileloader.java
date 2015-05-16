7cf8571a59592bf2cb54c48cb732cfa9828933d6
Innocentius <lzhao49@wisc.edu>
2015-05-16 09:47:20 +0800

    Create fileloader class

---
+++ cjfw/www/innocentius/net/cjfw/Fileloader.java
@@ -0,0 +1,23 @@
+package innocentius.net.cjfw;
+
+import java.io.File;
+
+public class Fileloader {
+	/**
+	 * only load one file into the directory.
+	 */
+	File a;
+	public Fileloader()
+	{
+	  a = new File("CJFWdata.txt");
+	  readfile("CJFWdata.txt");
+	}
+	/**
+	 * 
+	 * @param path
+	 */
+	private void readfile(String path) {
+		
+		
+	}
+}
